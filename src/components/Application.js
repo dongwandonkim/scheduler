a6700d4d40dc2733c393706a1ac8fc788990b48c
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-06-15 17:41:22 -0700

    axios request in application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,68 +1,62 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-import "components/Application.scss";
-import DayList from "./DayList";
-import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import 'components/Application.scss';
+import DayList from './DayList';
+import Appointment from 'components/Appointment';
 
 const appointments = {
   1: {
     id: 1,
-    time: "12pm",
+    time: '12pm',
   },
   2: {
     id: 2,
-    time: "1pm",
+    time: '1pm',
     interview: {
-      student: "Lydia Miller-Jones",
+      student: 'Lydia Miller-Jones',
       interviewer: {
         id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
       },
     },
   },
   3: {
     id: 3,
-    time: "2pm",
+    time: '2pm',
   },
   4: {
     id: 4,
-    time: "3pm",
+    time: '3pm',
     interview: {
-      student: "Archie Andrews",
+      student: 'Archie Andrews',
       interviewer: {
         id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
+        name: 'Cohana Roy',
+        avatar: 'https://i.imgur.com/FK8V841.jpg',
       },
     },
   },
   5: {
     id: 5,
-    time: "4pm",
+    time: '4pm',
+  },
+  6: {
+    id: 6,
+    time: '5pm',
   },
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("");
+  const [day, setDay] = useState('');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days').then((res) => {
+      setDays(res.data);
+    });
+  });
 
   return (
     <main className="layout">
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,13 @@
-import React, { Fragment } from "react";
-import Header from "./Header";
-import Show from "./Show";
-import Empty from "./Empty";
+import React from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
-import "./styles.scss";
+import './styles.scss';
 
 export default function Appointment(props) {
   return (
-    <Fragment>
+    <article className="appointment">
       <Header time={props.time} />
       {props.interview ? (
         <Show
@@ -17,6 +17,6 @@ export default function Appointment(props) {
       ) : (
         <Empty />
       )}
-    </Fragment>
+    </article>
   );
 }
